60b4e71476efade47c59c631f6632fe99ed1b9fd
mbalvanera <mbalvanera@otis.edu>
2017-06-04 23:00:04 -0700

    June Events
    1.1

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function(){
+  $(".logo").draggable();
+});
+
+$(function(){
+  $(".marqueeimg").draggable();
+});
+
+$(function(){
+  $(".holly").draggable();
+});
+
+$(function(){
+  $(".daniel").draggable();
+});
+
+$(function(){
+  $(".donatenow").draggable();
+});
+
+$(function(){
+  $(".instructions").draggable();
+});
+
+
+$(document).ready(function(c) {
+	$('.alert-close').on('click', function(c){
+		$(this).parent().fadeOut('slow', function(c){
+		});
+	});	
+});
